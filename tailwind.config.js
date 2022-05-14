8deddf2d45ac0aee41502bb77ca17d0dfa21dab9
Victoryyyyy <93453319+Victoryyyyy@users.noreply.github.com>
2022-05-14 07:25:32 +0100

    initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./*.html"],
+  theme: {
+    screens: {
+      sm: "375px", 
+      md: "768px",
+      lg: "976px",
+      xl: "1440px"
+      
+    },
+    extend: {
+      //  backgroundImage: {
+      //   sm: "url('/images/bg-desktop.svg')",
+      //   md: "url('/images/bg-desktop.svg')"
+    },
+  },
+  plugins: [],
+}
